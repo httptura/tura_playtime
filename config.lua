2c56b394ebc4dac4f0d5b23b4d5bc8ca54156032
tura.wav <fpslaze@gmail.com>
2025-06-01 13:11:25 +0200

    Update config.lua

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Rewards = {
+    [10] = {
+        items = {bread = 10, water = 10},
+        money = 50.00
+    },
+    [20] = {
+        items = {bread = 10, water = 10},
+        money = 50.00
+    },
+}
